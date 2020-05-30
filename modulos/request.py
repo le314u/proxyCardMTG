43a3256900c22ba1287101ceabe7ed7f1a270bf4
le314u <lucasmfpiu@gmail.com>
2020-05-30 07:50:17 -0300

    Fixed virtualEnv

--- modulos/request.py
+++ modulos/request.py
@@ -9,7 +9,6 @@ class request:
         #Cabeçalho de requisição get
         self.headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
         self.verbose=verbose
-    
 
     def downloadHTML(self, url):
         #Faz download da pagina

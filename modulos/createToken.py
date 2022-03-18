8c52a7169fd47da8d2916c9eb668edb01be467da
Lucas Mateus <lucasmfpiu@gmail.com>
2022-03-18 18:31:24 -0300

    fix toString do deck

--- modulos/createToken.py
+++ modulos/createToken.py
@@ -5,7 +5,9 @@ deck = {"Tokens": []}
 pasta = './'
 for diretorio, subpastas, arquivos in os.walk(pasta):
     for arquivo in arquivos:
-        deck["Tokens"].append( {"qtd": 1,"name": "token_"+arquivo,"url": "repositorio","img": arquivo} );
+        deck["Tokens"].append( {"qtd": 1,"name": "token_"+arquivo,"url": "repositorio","img": arquivo.split(".")[0]} );
 
 f=open("token.deck", 'w+')
-f.write(deck)
+f.write(str(deck))
+f.close()
+
